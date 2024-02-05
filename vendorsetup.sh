fc97916e65e01a190b470ef2ebeb096bf8471d2f
mnrniloy <mnrniloy0@gmail.com>
2024-02-05 12:34:07 +0600

    guacamole: Update dependencies

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,7 @@
 # Clone/Fetch Upstream Device Dependencies
 # Device Tree
 echo "Cloning sm8150-Common Tree"
-git clone https://github.com/mnrniloy/device_oneplus_sm8150-common.git -b 14 device/oneplus/sm8150-common
+git clone https://github.com/mnrniloy/TWRP_device_oneplus_sm8150-common.git -b 14-twrp device/oneplus/sm8150-common
 echo ""
 
 echo "Cloning kernel tree"
@@ -12,21 +12,3 @@ echo "Cloning Hardware"
 git clone https://github.com/mnrniloy/hardware_oneplus.git -b 14 hardware/oneplus
 echo ""
 
-echo "Cloning Vendor blobs"
-git clone https://github.com/mnrniloy/vendor_oneplus_guacamole.git vendor/oneplus/guacamole
-git clone https://github.com/mnrniloy/vendor_oneplus_sm8150-common.git -b 14 vendor/oneplus/sm8150-common
-echo ""
-
-# Qcom components
-echo "Cloning Qcom components"
-git clone https://github.com/yaap/device_qcom_common.git -b fourteen device/qcom/common
-git clone https://gitlab.com/yaosp/vendor_qcom_common.git -b fourteen vendor/qcom/common
-git clone https://github.com/mnrniloy/device_qcom_qssi.git device/qcom/qssi
-git clone https://github.com/mnrniloy/vendor_qcom-opensource_core-utils.git vendor/qcom/opensource/core-utils
-echo ""
-
-#  Apps
-echo "Cloning Apps"
-git clone https://github.com/KProfiles/android_packages_apps_Kprofiles packages/apps/Kprofiles
-git clone https://gitlab.com/nrniloy369/vendor_oneplus_apps.git vendor/oneplus/apps
-echo ""
